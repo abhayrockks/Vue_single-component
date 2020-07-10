0531c21383da324760e41e00af095d1fa25b7301
Abhay Mehta <abhayrockks@gmail.com>
2020-07-10 18:31:26 +0530

    second commit with error in assignment1_alert

---
+++ Add_edit_delete_list/src/components/Test.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="testing">
+    <h1>Let's make a table list</h1>
+
+    <i>Press enter</i>
+    <br />
+    <form v-on:submit.prevent>
+      <input v-on:keypress="submit" type="text" v-model="name" />
+    </form>
+    <ul>
+      <li v-for="(item, index) in data" v-bind:key="index">
+        <span>{{item}}</span>
+
+        <a href="#" v-on:click.prevent="deleteItem(index)">Delete</a>
+        <a href="#" v-on:click.prevent="editItem(index)">Edit</a>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+import Vue from "vue";
+
+export default {
+  name: "Test",
+  data: function() {
+    return {
+      name: "",
+      data: [],
+      editIndex: false
+    };
+  },
+  methods: {
+    submit: function(e) {
+      if (e.keyCode === 13) {
+        if (this.editIndex) {
+          // this.data.splice(this.editIndex,1,this.name);
+          //split also works fine, but just trying out Vue.set
+          Vue.set(this.data, this.editIndex, this.name);
+          this.name = "";
+          this.editIndex = -1;
+        } else {
+          this.data.push(this.name);
+          this.name = "";
+        }
+      }
+    },
+    deleteItem: function(index) {
+      this.data.splice(index, 1);
+    },
+    editItem: function(index) {
+      this.editIndex = index;
+      this.name = this.data[index];
+    }
+  }
+};
+</script>
+
+<style>
+.testing li {
+  border: 1px solid;
+  padding: 5px;
+}
+
+.testing li a {
+  font-size: 10px;
+  float: right;
+  padding: 5px;
+}
+</style>
---
+++ Vue_array_font-color/src/components/Test.vue
@@ -0,0 +1,41 @@
+<template>
+  <div id="testing">
+    <div v-bind:style="{color: color_value.color}">{{name}}</div>
+    <br />
+    <input type="text" v-model="name" />
+    <button v-on:click="del">Clear</button>
+    <button @click="makeCapital">Caps</button>
+    <br />
+    <br />
+    <select v-model="color_value">
+      <option v-for="(col, index) in colors" v-bind:key="index" v-bind:value="{color: col}">{{col}}</option>
+    </select>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Test",
+  data: function() {
+    return {
+      name: "abhay",
+      date: false,
+      colors: ["red", "yellow", "black", "orange"],
+      color_value: {
+        color: "black"
+      }
+    };
+  },
+  methods: {
+    del: function() {
+      this.name = "";
+    },
+    makeCapital: function() {
+      this.name = this.name.toUpperCase();
+    }
+  }
+};
+</script>
+
+<style>
+</style>
